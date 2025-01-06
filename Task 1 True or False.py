715d504eb45f2907dbb79ad019ef73bf1f5526a6
divii15 <divyadaksha143@gmail.com>
2025-01-06 17:03:16 +0530

    My next task(1)(7)

--- Task 1 True or False.py
+++ Task 1 True or False.py
@@ -1,3 +1,9 @@
+#TASK 1
+#7)Write a code?input 1 or 0
+#if user enter 0 output will be False
+#if user enter 1 output will be True
+
+
 a=int(input("Enter the value: "))
 if a==0:
     print("Output will be False")
